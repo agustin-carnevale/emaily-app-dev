9c083f280b88ae948c88993c791707bff72f6bc8
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-08-22 15:35:59 -0300

    authentication flow completed

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,15 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user,done)=>{
+	done(null,user.id)
+});
+
+passport.deserializeUser((id,done)=>{
+	User.findById(id)
+	.then(user => done(null,user));
+});
+
 passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientID,
@@ -12,7 +21,19 @@ passport.use(
 		callbackURL: '/auth/google/callback'
 	},
 	(accessToken, refreshToken, profile, done) => {
-		new User({googleId: profile.id}).save();
-		console.log('\nPROFILE:\n',profile);
+		User.findOne({googleId: profile.id})
+		.then( existingUser =>{
+			if (existingUser){
+				// We already have a record with the given profile ID
+				done(null, existingUser);
+				//console.log('\nExisting user =)\n');
+			}else{
+				// We don't have a user record with this profile ID, make a new record
+				new User({googleId: profile.id})
+				.save()
+				.then(user=>done(null,user));
+				//console.log('\nNEW USER CREATED!!\n');
+			}
+		});
 	})
 );
