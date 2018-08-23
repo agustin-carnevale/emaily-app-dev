0412ead2305dd813d7f0157c375ab417ddd5fc34
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-08-23 02:21:43 -0300

    creation/integration of the front-end (client)

--- services/passport.js
+++ services/passport.js
@@ -21,20 +21,16 @@ passport.use(
 		callbackURL: '/auth/google/callback',
 		proxy:true
 	},
-	(accessToken, refreshToken, profile, done) => {
-		User.findOne({googleId: profile.id})
-		.then( existingUser =>{
-			if (existingUser){
-				// We already have a record with the given profile ID
-				done(null, existingUser);
-				//console.log('\nExisting user =)\n');
-			}else{
-				// We don't have a user record with this profile ID, make a new record
-				new User({googleId: profile.id})
-				.save()
-				.then(user=>done(null,user));
-				//console.log('\nNEW USER CREATED!!\n');
-			}
-		});
+	async (accessToken, refreshToken, profile, done) => {
+		const existingUser = await User.findOne({googleId: profile.id});
+		
+		if (existingUser){
+			// We already have a record with the given profile ID
+			done(null, existingUser);
+		}else{
+			// We don't have a user record with this profile ID, make a new record
+			const user = await new User({googleId: profile.id}).save();
+			done(null,user);
+		}
 	})
 );
