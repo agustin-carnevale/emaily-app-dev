8692ac4e34f01a7ecb0e9df2ff44f38a0bab0d35
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-08-28 00:53:15 -0300

    survey creation (backend), sendGrid integration

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,12 @@ import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
+// For testing development (delete this after)
+//--------------------------
+import axios from 'axios';
+window.axios = axios;
+// -------------------------
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
