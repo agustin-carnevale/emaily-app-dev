5663ec739d2f73ef1aae9cfbe6521c1f653e4bab
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-08-30 03:14:04 -0300

    fetching and showing the surveys at the frontend

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import SurveysList from './surveys/SurveysList';
 
 class Dashboard extends Component{
 
     render() {
         return (
             <div>
-                <h1>Dashboard</h1>
-                <h3>Take a look at the results..</h3>
+                <h3>Dashboard</h3>
+                <h5>Take a look at the results so far..</h5>
+               
+                <SurveysList />
 
                 <Link to='/surveys/new' className="btn-floating btn-large waves-effect waves-light red right"><i className="material-icons">add</i></Link>
             </div>
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,6 +11,12 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
+    app.get('/api/surveys', requireLogin, async (req,res) => {
+        const surveys = await Survey.find({_user: req.user.id}).select({recipients:false});
+        res.send(surveys);
+    });
+
+
     app.get('/api/surveys/:surveyId/:choice',(req,res)=>{
         res.send('Thanks for voting!!');
     });
@@ -44,8 +50,7 @@ module.exports = app => {
     });
 
 
-    app.post('/api/surveys', requireLogin, requireCredits,
-        async (req,res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req,res) => {
             const {title, subject, body, recipients} = req.body;
             const survey = new Survey({
                 title,
